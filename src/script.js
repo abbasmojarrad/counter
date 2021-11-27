c9e5aaf61ab77222ae4c578da80c468be8aebec0
abbas_mojarrad <abbasmojarrad7@gmail.com>
2021-11-27 11:50:09 +0330

    add logic

--- src/script.js
+++ src/script.js
@@ -1,4 +1,31 @@
 const $ = document
-const increaseBtn = $.querySelector(".increase")
-const decreaseBtn = $.querySelector(".decrease")
-const resetBtn = $.querySelector(".reset")
+const increaseBtn = $.querySelector('.increase')
+const decreaseBtn = $.querySelector('.decrease')
+const resetBtn = $.querySelector('.reset')
+const number = $.querySelector('.number')
+let count = 0
+checkColor()
+function checkColor () {
+  if (count < 10) {
+    number.style.color = 'red'
+  } else {
+    number.style.color = 'green'
+  }
+}
+increaseBtn.addEventListener('click', function () {
+  count++
+  number.innerHTML = count
+  checkColor()
+})
+decreaseBtn.addEventListener('click', function () {
+  if (count > 0) {
+    count--
+    number.innerHTML = count
+    checkColor()
+  }
+})
+resetBtn.addEventListener('click', function () {
+  count = 0
+  number.innerHTML = count
+  checkColor()
+})
